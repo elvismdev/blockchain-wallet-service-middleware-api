5e937a6ab619f169cb0fb024b61d844d511d14b2
Elvis Morales <elvismdev@gmail.com>
2018-03-24 00:39:59 -0700

    JWT auth.

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Ami\BlockchainBundle\AmiBlockchainBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
