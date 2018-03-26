a4c9651b312a96302820283ec0bd678150686337
Elvis Morales <elvismdev@gmail.com>
2018-03-26 03:11:32 -0700

    Adding API endpoint to return whole wallet balance.

--- src/Controller/BlockchainMiddlewareController.php
+++ src/Controller/BlockchainMiddlewareController.php
@@ -5,19 +5,28 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Blockchain\Blockchain;
 
 class BlockchainMiddlewareController extends Controller
 {
     /**
-     * @Route("/blockchain/middleware", name="blockchain_middleware")
+     * @var Blockchain
      */
-    public function index()
+    private $blockchain;
+
+
+    public function __construct( array $walletCreds, Blockchain $blockchain )
     {
-    	$blockchain = $this->container->get('ami_blockchain.blockchain');
-    	$blockchain->Wallet->credentials('wallet-id-1', 'password-1');
+        $this->blockchain = $blockchain;
+        $this->blockchain->Wallet->credentials($walletCreds['id'], $walletCreds['main_password'], $walletCreds['second_password']);
+    }
 
-        $data = $blockchain->Wallet->getBalance();
-
-        return new JsonResponse($data);
+    /**
+     * Get the balance of the whole wallet.
+     * @Route("/balance", name="balance")
+     */
+    public function balance()
+    {
+        return new JsonResponse($this->blockchain->Wallet->getBalance());
     }
 }
